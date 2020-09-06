b076b87de2748ddccee2f557dcacf24c7926b773
rwik <rwikee@gmail.com>
2020-09-06 20:59:41 +0530

    Initial commit

---
+++ MongoDB-connection-using-Node-JS-master/DB/Conncection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const URI ="";
+
+const connectDB = async () => {
+  await mongoose.connect(URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  });
+  console.log('db connected..!');
+};
+
+module.exports = connectDB;
